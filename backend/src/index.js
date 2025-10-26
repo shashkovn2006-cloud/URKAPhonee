6beb9a4c7784e2539ecdc931eb39d7184ad423c7
shashkovn2006-cloud <shashkov.n2006@gmail.com>
2025-10-27 01:00:11 +0500

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const { testConnection } = require('./config/database');
+//const { testConnection } = require('./config/database');
 require('dotenv').config();
 
 const app = express();
@@ -82,3 +82,4 @@ process.on('SIGINT', async () => {
   });
 
 });
+
